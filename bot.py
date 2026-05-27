3dd6f4da1b0ba306616c1c2719cb0b41ea5a8a1d
ngatz197 <mngatia@protonmail.com>
2026-05-27 04:10:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -225,8 +225,9 @@ class PendingLimitBuy:
 class RobustBalanceManager:
     USDC_ADDRESS = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
     POLYGON_RPCS = [
-        "https://polygon-rpc.com",
-        "https://rpc.ankr.com/polygon",
+        "https://polygon-bor-rpc.publicnode.com",
+        "https://polygon.llamarpc.com",
+        "https://polygon.drpc.org",
     ]
 
     def __init__(self):
